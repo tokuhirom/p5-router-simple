0da153c3d6e3fef525e618febb11709af7d37935
tokuhirom <tokuhirom@gmail.com>
2010-03-17 08:36:17 +0900

    initial import

---
+++ lib/Router/Simple.pm
@@ -0,0 +1,39 @@
+package Router::Simple;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Router::Simple -
+
+=head1 SYNOPSIS
+
+  use Router::Simple;
+
+=head1 DESCRIPTION
+
+Router::Simple is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
