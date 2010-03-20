b32fb63d4edcba1792f9f2b79140cd36c10259af
tokuhirom <tokuhirom@gmail.com>
2010-03-20 17:22:34 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index eb942c9..8916f99 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,11 @@
     Revision history for Perl extension Router::Simple
    
    +0.04
    +
    +        (no feature changes)
    +        - some doc fixed(miyagawa++)
    +        - added test case(miyagawa++)
    +
     0.03
    
             [INCOMPATIBLE CHANGES]

--- lib/Router/Simple.pm
+++ lib/Router/Simple.pm
@@ -2,7 +2,7 @@ package Router::Simple;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 use Router::Simple::SubMapper;
 use List::Util qw/max/;
 use Carp ();
