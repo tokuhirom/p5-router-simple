ecdb7793e08c05835bd14c72725f651b1c90c81d
tokuhirom <tokuhirom@gmail.com>
2010-06-14 17:22:28 +0900

    Checking in changes prior to tagging of version 0.06.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 015822b..138b011 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Router::Simple
    
    +0.06
    +
    +        - require Test::More 0.88 or later for using done_testing.
    +          (reported by kazeburo++)
    +
     0.05
    
             [FEATURE ENHANCEMENTS]

--- lib/Router/Simple.pm
+++ lib/Router/Simple.pm
@@ -2,7 +2,7 @@ package Router::Simple;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 use Router::Simple::SubMapper;
 use Router::Simple::Route;
 use List::Util qw/max/;
