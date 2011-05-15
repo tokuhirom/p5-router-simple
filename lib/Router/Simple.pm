7a39a60d311617cdab567ca6440633cea744c215
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-05-15 11:11:15 +0900

    Checking in changes prior to tagging of version 0.09.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 3340bed..df8bbaa 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Router::Simple
    
    +0.09
    +
    +        - modernize package(no feature changes)
    +
     0.08
    
             - fix "Can't use an undefined value as a HASH reference" error for $route->{dest}

--- lib/Router/Simple.pm
+++ lib/Router/Simple.pm
@@ -2,7 +2,7 @@ package Router::Simple;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 use Router::Simple::SubMapper;
 use Router::Simple::Route;
 use List::Util qw/max/;
