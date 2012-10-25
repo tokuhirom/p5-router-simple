835ee8a46d72d41912af530803afe6eaf8c1ba1a
tokuhirom <tokuhirom@gmail.com>
2012-10-25 16:40:45 +0900

    Checking in changes prior to tagging of version 0.12.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index f36e2d8..955fb64 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Router::Simple
    
    +0.12
    +
    +        - minor pod fix
    +          (book++)
    +
     0.11
    
             - Added warnings when regexp contains parens.

--- lib/Router/Simple.pm
+++ lib/Router/Simple.pm
@@ -2,7 +2,7 @@ package Router::Simple;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 use Router::Simple::SubMapper;
 use Router::Simple::Route;
 use List::Util qw/max/;
